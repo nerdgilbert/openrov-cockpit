ea2ab4dccf7f44ab5b8083cc3cbc6f0a0baddba3
Charles Cross <charles@openrov.com>
2016-05-05 21:52:15 -0700

    Removed all linux files not related to cockpit. This preempts the platform abstraction layer system plugin.

--- install_lib/openrov-cockpit-afterinstall.sh
+++ install_lib/openrov-cockpit-afterinstall.sh
@@ -7,14 +7,14 @@ ln -s /opt/openrov/cockpit/linux/openrov.service /etc/init.d/openrov
 chmod +x /opt/openrov/cockpit/linux/openrov.service
 update-rc.d openrov defaults
 
-chmod +x /opt/openrov/cockpit/linux/rc.local
+#chmod +x /opt/openrov/cockpit/linux/rc.local
 
 chown -R rov /opt/openrov/cockpit
 chgrp -R admin /opt/openrov/cockpit
 
 # setup reset and uart for non black BB
-cp /etc/rc.local /etc/rc.local_orig
-cat > /etc/rc.local << __EOF__
+#cp /etc/rc.local /etc/rc.local_orig
+#cat > /etc/rc.local << __EOF__
 #!/bin/bash -e
 #
 # rc.local
